2e2ab6cf6c73445a9da647699d39021322716cbc
Xoferek <40631337+Xoferek@users.noreply.github.com>
2026-02-23 23:00:27 +0100

    Update graphicalapplication.cpp

--- src/framework/core/graphicalapplication.cpp
+++ src/framework/core/graphicalapplication.cpp
@@ -258,7 +258,7 @@ void GraphicalApplication::run()
             m_framebuffer->resize(g_painter->getResolution());
             m_framebuffer->bind();
         }
-#ifndef ANDROID
+
 
         if (toDrawMapQueue && toDrawMapQueue->hasFrameBuffer()) {
             AutoStat s(STATS_RENDER, "UpdateMap");
@@ -268,8 +268,7 @@ void GraphicalApplication::run()
             toDrawMapQueue->draw(DRAW_ALL);
             m_mapFramebuffer->release();
         }
-#else
-#endif
+
         {
             AutoStat s(STATS_RENDER, "Clear");
             g_painter->clear(Color::alpha);
